73191526b9bd21cd4257b5b1d46fc435bf32baee
toon nelissen appsaloon <toon@appsaloon.be>
2019-10-07 18:38:23 +0200

    Updates

---
+++ precache-manifest.dd5feba0138b956589a1057b111cd7ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd079c63f8e04bf20ce54029750050cb",
+    "url": "/seat61.com-map/index.html"
+  },
+  {
+    "revision": "57c8055c0223731bb2f7",
+    "url": "/seat61.com-map/static/css/main.83d2b961.chunk.css"
+  },
+  {
+    "revision": "f8eb656dfee88bc1e18c",
+    "url": "/seat61.com-map/static/js/2.0e385aa6.chunk.js"
+  },
+  {
+    "revision": "57c8055c0223731bb2f7",
+    "url": "/seat61.com-map/static/js/main.3f4be928.chunk.js"
+  },
+  {
+    "revision": "9bb72fabdf917490cb2c",
+    "url": "/seat61.com-map/static/js/runtime~main.926508a6.js"
+  }
+]);
